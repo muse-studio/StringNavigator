62158f78843aa1eaa981539e57b74848510a6f26
haruka <32345030@fukuchiyama.ac.jp>
2026-07-14 12:57:19 +0900

    UIウィンドウ作成

---
+++ ui/main_window.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+
+def show_main_window():
+    root = tk.Tk()
+    root.title("StringNavigator")
+
+    root.geometry("700x600")
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    show_main_window()
